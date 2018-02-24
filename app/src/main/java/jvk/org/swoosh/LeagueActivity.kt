a8e441166d7673bf7b3c8475a827b1b23d37512f
Jan Valentik <jvalentik@sk.ibm.com>
2018-02-24 03:18:51 +0100

    feat(layout): add league activity

---
+++ app/src/main/java/jvk/org/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package jvk.org.swoosh
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
